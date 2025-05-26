02bb976a3b4caea88c77a7696b21c167d25256da
AJAY SRI SAI V K <156649716+Ajvj@users.noreply.github.com>
2025-05-26 20:24:49 +0530

    Delete Operator Precedence .java

--- Operator Precedence .java
+++
@@ -1,7 +0,0 @@
-Operator Precedence program:
-public class OperatorPrecedence {
-    public static void main(String[] args) {
-        int result = 10 + 5 * 2;
-        System.out.println("Result = " + result);
-    }
-}
